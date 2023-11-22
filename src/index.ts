a21a5f76931a58d01f1fbf6ca35ebc106e8996b8
Silk Rose <silkrose@love-tolerance.com>
2023-11-22 02:56:24 -0500

    Run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import * as fs from 'fs/promises';
+import * as fs from "fs/promises";
 
-import config from '../config.json';
-import { live } from './live';
-import { join } from 'path';
-import yargs = require('yargs');
+import config from "../config.json";
+import { live } from "./live";
+import { join } from "path";
+import yargs = require("yargs");
 
 export interface Arguments {
-    live: boolean;
-    search: string;
+	live: boolean;
+	search: string;
 }
 
 const argv: Arguments = yargs.options({
-    live: { type: 'boolean', default: false, alias: 'l' },
-    search: { type: 'string', default: '', alias: 's' }
+	live: { type: "boolean", default: false, alias: "l" },
+	search: { type: "string", default: "", alias: "s" },
 }).argv;
 
 let outputPath: string = config.outputPath;
-if (!config.outputPath.startsWith('/')) {
-    outputPath = join(__dirname, '..', config.outputPath);
+if (!config.outputPath.startsWith("/")) {
+	outputPath = join(__dirname, "..", config.outputPath);
 }
 
 export const outputPaths = {
-    output: outputPath,
-    images: join(outputPath, 'images'),
-    metadata: join(outputPath, 'metadata'),
-    commentMetadata: join(outputPath, 'metadata', 'comments'),
-    imageMetadata: join(outputPath, 'metadata', 'images')
+	output: outputPath,
+	images: join(outputPath, "images"),
+	metadata: join(outputPath, "metadata"),
+	commentMetadata: join(outputPath, "metadata", "comments"),
+	imageMetadata: join(outputPath, "metadata", "images"),
 };
 
 async function main() {
-    await fs.mkdir(outputPaths.output, { recursive: true });
-    await fs.mkdir(outputPaths.images, { recursive: true });
-    await fs.mkdir(outputPaths.metadata, { recursive: true });
-    await fs.mkdir(outputPaths.commentMetadata, { recursive: true });
-    await fs.mkdir(outputPaths.imageMetadata, { recursive: true });
+	await fs.mkdir(outputPaths.output, { recursive: true });
+	await fs.mkdir(outputPaths.images, { recursive: true });
+	await fs.mkdir(outputPaths.metadata, { recursive: true });
+	await fs.mkdir(outputPaths.commentMetadata, { recursive: true });
+	await fs.mkdir(outputPaths.imageMetadata, { recursive: true });
 
-    if (argv.live) {
-        return live(argv);
-    }
+	if (argv.live) {
+		return live(argv);
+	}
 }
 
 main().catch(console.error);
--- src/utils.ts
+++ src/utils.ts
@@ -1,67 +1,104 @@
-import fs from 'fs/promises';
-import { createWriteStream } from 'fs';
-import { extname, join } from 'path';
-import { outputPaths } from './index';
-import { Image } from './interfaces';
-import config from '../config.json';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import * as fetch from 'node-fetch';
+import fs from "fs/promises";
+import { createWriteStream } from "fs";
+import { extname, join } from "path";
+import { outputPaths } from "./index";
+import { Image } from "./interfaces";
+import config from "../config.json";
+import { pipeline } from "stream";
+import { promisify } from "util";
+import * as fetch from "node-fetch";
 
 const streamPipeline = promisify(pipeline);
 
-export async function fetchMetadata(id): Promise<Image> {
-    return await fetch(`http${config.https && 's'}://${config.origin}/api/v1/json/images/${id}?key=${config.apiKey}`)
-        .then(res => res.json())
-        .then(res => res.image)
-        .then((image: Image) => {
-            image.created_at = new Date(image.created_at);
-            image.updated_at = new Date(image.updated_at);
-            image.first_seen_at = new Date(image.first_seen_at);
-            return image;
-        })
-        .catch(err => err);
+export async function fetchMetadata(id: number): Promise<Image> {
+	return await fetch(
+		`http${config.https && "s"}://${
+			config.origin
+		}/api/v1/json/images/${id}?key=${config.apiKey}`
+	)
+		.then((res: { json: () => any }) => res.json())
+		.then((res: { image: any }) => res.image)
+		.then((image: Image) => {
+			image.created_at = new Date(image.created_at);
+			image.updated_at = new Date(image.updated_at);
+			image.first_seen_at = new Date(image.first_seen_at);
+			return image;
+		})
+		.catch((err: any) => err);
 }
 
-export async function getImageMetadata(id: number, fetchIfMissing: boolean = true) {
-    return await fs.readFile(join(outputPaths.imageMetadata, `${id}.json`), 'utf8')
-        .then(JSON.parse, async err => {
-            if (err.code === 'ENOENT' && fetchIfMissing) {
-                // Assume we can fetch the metadata now
-                return await fetchMetadata(id);
-            }
-        });
+export async function getImageMetadata(
+	id: number,
+	fetchIfMissing: boolean = true
+) {
+	return await fs
+		.readFile(join(outputPaths.imageMetadata, `${id}.json`), "utf8")
+		.then(JSON.parse, async (err) => {
+			if (err.code === "ENOENT" && fetchIfMissing) {
+				// Assume we can fetch the metadata now
+				return await fetchMetadata(id);
+			}
+		});
 }
 
 export async function saveImageMetadata(image: Image) {
-    return fs.writeFile(join(outputPaths.imageMetadata, `${image.id}.json`), JSON.stringify(image), 'utf8');
+	return fs.writeFile(
+		join(outputPaths.imageMetadata, `${image.id}.json`),
+		JSON.stringify(image),
+		"utf8"
+	);
 }
 
-export async function downloadImage(id: number, metadata?: Image, iteration: number = 1) {
-    // Rare that we'd need to retry
-    if (iteration > 1) {
-        if (iteration > config.maxDownloadAttempts) return;
-        console.warn(`Retrying image download for ${id || metadata?.id}; attempt ${iteration}/${config.maxDownloadAttempts}`);
-    }
-    metadata = metadata || await fetchMetadata(id);
-    if (metadata.representations?.full == null) {
-        setTimeout(async () => {
-            console.error(`Could not find full image representation for ${id}}.`);
-            await downloadImage(id, metadata, iteration + 1);
-        }, config.downloadAttemptRetryTime);
-        return;
-    }
-    const image = await fetch(metadata.representations.full).catch(err => err);
-    if (image == null || image instanceof Error || image.toString().startsWith('<html>')) {
-        setTimeout(async () => {
-            const reason = image == null ? 'Image is null' : image instanceof Error ? 'Image is error' : image.toString().startsWith('<html>') ? 'Image is HTML' : 'Unknown';
-            console.error(`Could not get image ${id}. (${reason})`);
-            await downloadImage(id, metadata, iteration + 1);
-        }, config.downloadAttemptRetryTime);
-        return;
-    }
-    const extension = extname(metadata.representations.full);
-    const writeStream = createWriteStream(join(outputPaths.images, `${metadata.id}${extension}`));
-    // @ts-ignore
-    await streamPipeline(image.body, writeStream);
+export async function downloadImage(
+	id: number,
+	metadata?: Image,
+	iteration: number = 1
+) {
+	// Rare that we'd need to retry
+	if (iteration > 1) {
+		if (iteration > config.maxDownloadAttempts) return;
+		console.warn(
+			`Retrying image download for ${
+				id || metadata?.id
+			}; attempt ${iteration}/${config.maxDownloadAttempts}`
+		);
+	}
+	metadata = metadata || (await fetchMetadata(id));
+	if (metadata.representations?.full == null) {
+		setTimeout(async () => {
+			console.error(
+				`Could not find full image representation for ${id}}.`
+			);
+			await downloadImage(id, metadata, iteration + 1);
+		}, config.downloadAttemptRetryTime);
+		return;
+	}
+	const image = await fetch(metadata.representations.full).catch(
+		(err: any) => err
+	);
+	if (
+		image == null ||
+		image instanceof Error ||
+		image.toString().startsWith("<html>")
+	) {
+		setTimeout(async () => {
+			const reason =
+				image == null
+					? "Image is null"
+					: image instanceof Error
+					? "Image is error"
+					: image.toString().startsWith("<html>")
+					? "Image is HTML"
+					: "Unknown";
+			console.error(`Could not get image ${id}. (${reason})`);
+			await downloadImage(id, metadata, iteration + 1);
+		}, config.downloadAttemptRetryTime);
+		return;
+	}
+	const extension = extname(metadata.representations.full);
+	const writeStream = createWriteStream(
+		join(outputPaths.images, `${metadata.id}${extension}`)
+	);
+	// @ts-ignore
+	await streamPipeline(image.body, writeStream);
 }
